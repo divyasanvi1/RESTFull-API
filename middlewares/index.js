c5e406126df56dd8a633f990731cf5ddb77811db
divyasanvi1 <sanvidiv22@gmail.com>
2025-03-09 11:35:39 +0530

    comments

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,7 +2,10 @@ const fs=require("fs");
 
 function logReqRes(filename){
     return (req,res,next)=>{
-        fs.appendFile("./log.txt",`\n${Date.now()}: ${req.ip}: ${req.method} : ${req.path}\n`,(err,data)=>{
+        fs.appendFile(filename,`\n${Date.now()}: ${req.ip}: ${req.method} : ${req.path}\n`,(err,data)=>{
+            //Closures allow inner functions to remember variables from their parent functions, even after the parent has returned. ex-filename
+            //The middleware still has access to filename = "./log.txt", even though logReqRes finished long ago. because this middleware gets returned to app.use when 
+            //server just started
             next();
         })
     };
